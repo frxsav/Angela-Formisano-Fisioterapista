0723f39a00deaf573792f6b263c1594b773f3e3e
frxsav <keccofor@gmail.com>
2025-11-21 18:07:03 +0100

    corrected CSP warning (3)

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const securityHeaders = [
-  // {
-  //   key: 'Content-Security-Policy',
-  //   value: `
-  //     default-src 'self';
-  //     script-src 'self';
-  //     style-src 'self';
-  //     img-src 'self' blob: data:;
-  //     font-src 'self';
-  //     object-src 'none';
-  //     base-uri 'self';
-  //     form-action 'self';
-  //     frame-ancestors 'none';
-  //     upgrade-insecure-requests;
-  //     require-trusted-types-for 'script';
-  //   `
-  //     .replace(/\s{2,}/g, ' ')
-  //     .trim(),
-  // },
+  {
+    key: 'Content-Security-Policy',
+    value: `
+      default-src 'self';
+      script-src 'self';
+      style-src 'self';
+      img-src 'self' blob: data:;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      upgrade-insecure-requests;
+      require-trusted-types-for 'script';
+    `
+      .replace(/\s{2,}/g, ' ')
+      .trim(),
+  },
   {
     key: 'Cross-Origin-Opener_Policy',
     value: 'same-origin',
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,22 @@
 import './globals.css';
-import CspNonceProvider from '@/providers/CspNonceProvider';
-// import { Spectral } from 'next/font/google';
-// import { Header, Footer } from '@/ui';
-// import { Providers } from '@/providers/ParallaxProvider';
-// import { headers } from 'next/headers';
+import { Spectral } from 'next/font/google';
+import { Header, Footer } from '@/ui';
+import { Providers } from '@/providers/ParallaxProvider';
 
-// const spectral = Spectral({
-//   subsets: ['latin'],
-//   weight: ['300'],
-//   variable: '--font-spectral',
-// });
+const spectral = Spectral({
+  subsets: ['latin'],
+  weight: ['300'],
+  variable: '--font-spectral',
+});
 
 export default function RootLayout({ children }) {
   return (
-    <CspNonceProvider>{children}</CspNonceProvider>
-    // <html lang="en" nonce={nonce} suppressHydrationWarning>
-    //   <body className={`${spectral.variable}`}>
-    //     <Header className="z-50" />
-    //     <Providers>{children}</Providers>
-    //     <Footer className="bottom-0 w-full" />
-    //   </body>
-    // </html>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${spectral.variable}`}>
+        <Header className="z-50" />
+        <Providers>{children}</Providers>
+        <Footer className="bottom-0 w-full" />
+      </body>
+    </html>
   );
 }
