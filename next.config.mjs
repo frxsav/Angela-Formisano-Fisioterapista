e6a35598c168b04aa5df358a703d34046377b5f5
frxsav <keccofor@gmail.com>
2025-11-21 18:09:23 +0100

    added unsafe-inline back

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: `
       default-src 'self';
-      script-src 'self';
-      style-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
       img-src 'self' blob: data:;
       font-src 'self';
       object-src 'none';
